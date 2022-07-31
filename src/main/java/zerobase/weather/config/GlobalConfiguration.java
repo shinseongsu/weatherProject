ac4e47af21c74c75aa3e27e25c5b73f1dcdb7c93
Shin seongsu <tlstjdtn321@naver.com>
2022-08-01 01:31:06 +0900

    feat: 문서작성 swagger 추가

---
+++ src/main/java/zerobase/weather/config/GlobalConfiguration.java
@@ -0,0 +1,11 @@
+package zerobase.weather.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableScheduling
+public class GlobalConfiguration {
+}
